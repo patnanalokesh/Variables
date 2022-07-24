764fb6583ccee73e25e510216aeb4671285f0e9f
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-24 13:15:33 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,47 @@
+# a=5
+# number=4
+# _=8
+# print(_)
+# print(number)
+# print(a)
+# print(g)
+
+# _com=4
+# _frod_=3
+# 6t=3
+# a b=4
+# v_g=5
+# adovkhnrumvhbvljbv=4
+# print(_com)
+# print(_frod_)
+# print(6t)
+# print(a b)
+# print(v_g)
+# print(adovkhnrumvhbvljbv)
+
+# A=6 ; B=7
+# print(A)
+# print(B)
+# C=A
+# A=B
+# B=C
+# print(A)
+# print(B)
+# print(C)4
+
+# x=8;y=5
+# print(x);print(y)
+# x,y=y,x
+# print(x);print(y)
+
+# print('abc'.isidentifier())
+# print('a b c'.isidentifier())
+# print('_'.isidentifier())
+# print('a_b_'.isidentifier())
+# print('a3b_'.isidentifier())
+# print('a3b_7'.isidentifier())
+# print('43b_'.isidentifi7er())
+# print('ab7_c'.isidentifi7er())
+# print('abc_7'.isidentifi7er())
+# print('a5c_5'.isidentifi7er())
+# print('abs_v'.isidentifier())
